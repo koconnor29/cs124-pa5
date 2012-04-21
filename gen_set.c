8732e60164a5a5a4880343f921fd8afc5b66477a
Kevin O'Connor <kevinoconnor29@gmail.com>
2012-04-21 15:38:43 -0400

    function to generate sets

---
+++ gen_set.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define big unsigned long long
+
+big r(void);
+
+int
+main(int argc, char *argv[])
+{
+  srand(time(NULL));
+  if (argc!=2)
+  {
+    printf("Usage: ./stdrep (size of set)\n");
+    return 1;
+  }
+  int n = atoi(argv[1]);
+  int i,j;
+  FILE *fp = fopen("set.txt","w");
+  for (j=0;j<n;j++)
+  {
+    big num = r();
+    fprintf(fp,"%llu\n",&num);
+  }
+  fclose(fp);
+  return 0;
+}
+
+
+big r()
+{
+  big x = 0;
+  int i;
+  for (i=0;i<12;i++)
+  {
+    x*=10;
+    x+=rand()%10;
+    x*=10;
+  }
+
+  return x;
+}
