aec83ea5ad1d8602479b276f7eda4ea8b8e79abf
Gurbir Singh Dhillon <dhillon@ice1.fas.harvard.edu>
2012-04-22 17:59:19 -0400

    neighbors

--- stdrep.c
+++ stdrep.c
@@ -4,8 +4,13 @@
 
 int *gen_rand_std(int n);
 
+int *neighbor(int* s);
+
+
+
 int *gen_rand_std(int n)
 {
+  //generate n (100) random plus or minus 1s
   srand(time(NULL));
   int *s = malloc(n*sizeof(int));
   int i;
@@ -15,3 +20,17 @@ int *gen_rand_std(int n)
   return s;
 }
 
+
+int *neighbor(int* s)
+{
+ //choose two random indices between 0 and  100
+ int i = rand() % 100;
+ int j = rand() % 100;
+
+ //switch s[i], s[j] with probability 1/2
+ s[i]*= (2*(rand() % 2) - 1); 
+ s[j]*= (2*(rand() % 2) - 1); 
+ 
+ //output modified s
+ return s;
+}
